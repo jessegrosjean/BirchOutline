b46f89fc1cfbe82d9d5daaee22a0e49f895d069a
Jesse Grosjean <jesse@hogbaysoftware.com>
2016-06-29 13:22:25 -0400

    String path util

---
+++ Common/Sources/String.swift
@@ -0,0 +1,46 @@
+//
+//  String.swift
+//  BirchOutline
+//
+//  Created by Jesse Grosjean on 6/28/16.
+//  Copyright © 2016 Jesse Grosjean. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var lastPathComponent: String {
+        get {
+            return (self as NSString).lastPathComponent
+        }
+    }
+    var pathExtension: String {
+        get {
+            return (self as NSString).pathExtension
+        }
+    }
+    var stringByDeletingLastPathComponent: String {
+        get {
+            return (self as NSString).stringByDeletingLastPathComponent
+        }
+    }
+    var stringByDeletingPathExtension: String {
+        get {
+            return (self as NSString).stringByDeletingPathExtension
+        }
+    }
+    var pathComponents: [String] {
+        get {
+            return (self as NSString).pathComponents
+        }
+    }
+    func stringByAppendingPathComponent(path: String) -> String {
+        let nsSt = self as NSString
+        return nsSt.stringByAppendingPathComponent(path)
+    }
+    
+    func stringByAppendingPathExtension(ext: String) -> String? {
+        let nsSt = self as NSString
+        return nsSt.stringByAppendingPathExtension(ext)
+    }
+}

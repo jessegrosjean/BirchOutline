4775ae790a0462be9db83f2f0aa21cbd521ca4a0
Jesse Grosjean <jesse@hogbaysoftware.com>
2016-07-10 14:11:13 -0400

    Expose Disposable type

---
+++ Common/Sources/DisposableType.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  BirchOutline
+//
+//  Created by Jesse Grosjean on 7/6/16.
+//  Copyright © 2016 Jesse Grosjean. All rights reserved.
+//
+
+import Foundation
+import JavaScriptCore
+
+public protocol DisposableType: AnyObject {
+    
+    func dispose()
+    
+}
+
+extension JSValue: DisposableType {
+
+    public func dispose() {
+        invokeMethod("dispose", withArguments: [])
+    }
+    
+}
